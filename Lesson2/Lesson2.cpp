62a55dcda8693dda531e6153a2166971829746b7
mirze <mirze@DESKTOP-H5NKI9E>
2024-07-11 10:58:31 +0400

    Add project files.

---
+++ Lesson2/Lesson2.cpp
@@ -0,0 +1,84 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "\tMotivation By Goat...\n";
+    cout << "In my mind I'm best\n";
+    cout << "I don't care what anyone thinks\n";
+    cout << "\t\t-Cristiano Ronaldo (GOAT)-\n";
+
+    cout << "_____________________________\n";
+    cout << "\tEV ELANI             \\\n";
+    cout << "_____________________________\\\n";
+    cout << " Narimanovda 3 otaqli ev    \\\n";
+    cout << "_____________________________\\\n";
+    cout << "Mohteshem menzile 18 ay     \\\n";
+    cout << "taksitle sahib olun         \\\n";
+    cout << "_____________________________\\\n";
+    cout << "QIYMET: 75.000              \\\n";
+    cout << "_____________________________\\\n";
+    cout << "Elaqe:099-869-19-99         \\\n";
+    cout << "_____________________________\\\n";
+    
+    double v = 0;
+    double t = 0;
+    double a = 0;
+
+    cout << "v: ";
+    cin >> v;
+    cout << "t: ";
+    cin >> t;
+    cout << "a: ";
+    cin >> a;
+
+    double s = v * t + (a * t * t) / 2;
+
+    cout << "S= ";
+    cout << s << endl;
+
+
+    double R1 = 0;
+    double R2 = 0;
+    double R3 = 0;
+    cout << "R1:";
+    cin >> R1;
+    cout << "R2:";
+    cin >> R2;
+    cout << "R3:";
+    cin >> R3;
+    double RO = 1 / (1 / R1 + 1 / R2 + 1 / R3);
+    cout << "RO:" << RO << endl;
+
+    double u;
+    const float pi = 3.14;
+    cout << "Cevrenin uzunlugunu qeyd edin: ";
+    cin >> u;
+    double d = u / pi;
+    cout << "Cevrenin diametri beraberdir: " << d << endl;
+
+    double tecil;
+    double t;
+    double v;
+    cout << "tecili qeyd edin: ";
+    cin >> tecil;
+    cout << "zamani qeyd edin: ";
+    cin >> t;
+    cout << "sureti qeyd edin: ";
+    cin >> v;
+    double s = (v * t) + (tecil * t * t) / 2;
+    cout << "S beraberdir: " << s << endl;
+
+    double qtm;
+    double msf;
+    cout << "qet edilen muddet: ";
+    cin >> qtm;
+    cout << "mesafeni qeyd edin: ";
+    cin >> msf;
+    double suret = msf / qtm;
+    cout << "Getme sureti: ";
+    cout << suret;
+
+    //tapsiriq 8i ve 9u yaza bilmedim:(
+}
+
